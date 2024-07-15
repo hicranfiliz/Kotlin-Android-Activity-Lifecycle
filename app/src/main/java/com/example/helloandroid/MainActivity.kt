6bd14745900f904d47f419b80c95026aaf15d866
hicranfiliz <h.filiz781@gmail.com>
2024-07-15 16:27:19 +0300

    second commit

--- app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -1,11 +1,119 @@
 package com.example.helloandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.example.helloandroid.ui.SecondActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.i("Mainactivity", "ONcreate")
+
         setContentView(R.layout.activity_main)
+
+        /*if (savedInstanceState != null){
+            val userField =  savedInstanceState.getString("userfield")
+            val edtUserField = findViewById<EditText>(R.id.edtUserField)
+            edtUserField.setText(userField)
+        }*/
+
+        val btnOpenSecondActivity = findViewById<Button>(R.id.btnopenSecondActivity)
+        btnOpenSecondActivity.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("key", "value")
+
+            val bundle = Bundle()
+            bundle.putString("keyBundle", "valueBundle")
+
+            intent.putExtras(bundle)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        Log.i("Mainactivity", "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.i("Mainactivity", "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.i("Mainactivity", "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("Mainactivity", "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.i("Mainactivity", "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.i("Mainactivity", "onRestart")
+        super.onRestart()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i("Mainactivity", "onSaveInstanceState")
+
+       /* val edtUserField = findViewById<EditText>(R.id.edtUserField)
+        val userField = edtUserField.text.toString()
+
+        outState.putString("userfield", userField) */
+
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.i("Mainactivity", "onRestoreInstanceState")
+
+        /*val userField =  savedInstanceState.getString("userfield")
+        val edtUserField = findViewById<EditText>(R.id.edtUserField)
+        edtUserField.setText(userField) */
+
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onBackPressed() {
+        Log.i("Mainactivity", "onBackPressed")
+
+        super.onBackPressed()
+    }
+
+    override fun onUserLeaveHint() {
+        Log.i("Mainactivity", "onUserLeaveHint")
+
+        super.onUserLeaveHint()
+    }
+
+    override fun onUserInteraction() {
+        Log.i("Mainactivity", "onUserInteraction")
+        super.onUserInteraction()
+    }
+
+    override fun onContentChanged() {
+        Log.i("Mainactivity", "onContentChanged")
+
+        super.onContentChanged()
+    }
+
+    override fun onLowMemory() {
+        Log.i("Mainactivity", "onLowMemory")
+
+        super.onLowMemory()
     }
 }
---
+++ app/src/main/java/com/example/helloandroid/ui/SecondActivity.kt
@@ -0,0 +1,107 @@
+package com.example.helloandroid.ui
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.helloandroid.R
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+
+        Log.i("second activity", "onCreateesecnd")
+
+        val param = intent.getStringExtra("key") ?: "default"
+
+        val bundle = intent.extras
+        val bundleParam = bundle?.getString("keyBundle" , "default bundle value") ?: "dlfk"
+        Log.i("knf", param)
+        Log.i("knf", bundleParam)
+    }
+
+
+    override fun onStart() {
+        Log.i("second activity", "onStartsecnd")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.i("second activity", "onResumesecnd")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.i("second activity", "onPausesecnd")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("second activity", "onStopsecnd")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.i("second activity", "onDestroysecnd")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.i("second activity", "onRestartsecnd")
+        super.onRestart()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.i("second activity", "onSaveInstanceState")
+
+        /* val edtUserField = findViewById<EditText>(R.id.edtUserField)
+         val userField = edtUserField.text.toString()
+
+         outState.putString("userfield", userField) */
+
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.i("second activity", "onRestoreInstanceState")
+
+        /*val userField =  savedInstanceState.getString("userfield")
+        val edtUserField = findViewById<EditText>(R.id.edtUserField)
+        edtUserField.setText(userField) */
+
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onBackPressed() {
+        Log.i("second activity", "onBackPressed")
+
+        super.onBackPressed()
+    }
+
+    override fun onUserLeaveHint() {
+        Log.i("second activity", "onUserLeaveHint")
+
+        super.onUserLeaveHint()
+    }
+
+    override fun onUserInteraction() {
+        Log.i("second activity", "onUserInteraction")
+        super.onUserInteraction()
+    }
+
+    override fun onContentChanged() {
+        Log.i("second activity", "onContentChanged")
+
+        super.onContentChanged()
+    }
+
+    override fun onLowMemory() {
+        Log.i("second activity", "onLowMemory")
+
+        super.onLowMemory()
+    }
+}
